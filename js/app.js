c784a1f1d757f3428140adb89e505edd544ba69e
fernandoxmartin <fernando@DESKTOP-ES6V7UH.localdomain>
2019-11-04 16:09:45 -0800

    updated design and responsiveness

--- js/app.js
+++ js/app.js
@@ -32,6 +32,9 @@ const links = document.querySelectorAll(".left-nav a");
 links.forEach(link => {
   link.addEventListener("click", function() {
     changeActive(this);
+    if (window.innerWidth < 600) {
+      sideNav.style.transform = "translate(-100%)";
+    }
   });
 });
 
@@ -44,5 +47,18 @@ function changeActive(e) {
   const home = document.querySelector(".logo a");
   home.addEventListener("click", () => {
     e.classList.remove("active-link");
+    if (window.innerWidth < 600) {
+      sideNav.style.transform = "translate(-100%)";
+    }
   });
 }
+
+const navOpen = document.querySelector(".top-nav-left i");
+const navClose = document.querySelector(".side-nav i");
+const sideNav = document.querySelector(".side-nav");
+navOpen.addEventListener("click", () => {
+  sideNav.style.transform = "translateX(0%)";
+});
+navClose.addEventListener("click", () => {
+  sideNav.style.transform = "translate(-100%)";
+});
